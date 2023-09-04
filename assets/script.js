54b3dbf32864450c13361790c47f3c43f23ec177
Antoni Castagne <anto.casatgne@hotmail.fr>
2023-09-04 12:10:05 +0200

    ajout nouvelle cont + position slide de départ

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
@@ -21,18 +21,22 @@ const left = document.querySelector(".arrow_left");
 const right = document.querySelector(".arrow_right");
 const point = document.querySelector(".dot");
 const img = document.querySelector(".banner-img");
-const txt = document.querySelector(".img-txt")
+const txt = document.querySelector(".img-txt");
 
-console.log(left, right, point, img, txt)
+let positionSlide =0;
+
+console.log(left, right, point, img, txt);
 
 left.addEventListener("click",() => {
 	if (slides == 0) {
 		slides = -1 } 
+		img.src = slides[positionSlide].image;
 		point.classList.add("dot_selected");
 });
 
 right.addEventListener("click",() => {
 	if (slides == 0) {
 	slides =  +1 }
+	img.src = slides[positionSlide].image;
 	point.classList.add("dot_selected");
 });
